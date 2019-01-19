a54327d467b035da3ca1a3bb6318bbdbf2eba03e
Flo <Flo@moringa.com>
2019-01-19 10:59:34 +0300

    Include a get function in the service component, for making get requests

--- src/app/services/repository.service.ts
+++ src/app/services/repository.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
 
 
 @Injectable({
@@ -8,12 +9,15 @@ import { HttpClient } from '@angular/common/http';
 export class RepositoryService {
 
 private username: string;
+private clientid = 'c24a38c68432f92b0049';
+private clientsecret = 'd020a5ef1ca89b17d25fb710beeb0baeafe6ad67';
 
   constructor(private http: HttpClient) {
     console.log("service is now ready!");
     this.username = 'Mukabana';
    }
-   getRepositoryInfo(){
-     return this.http.get("https://api.github.com/users/" + this.username);
+   getRepositorInfo(){
+     return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+     .map(res => res.json());
    }
 }
